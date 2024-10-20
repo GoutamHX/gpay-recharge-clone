bf68dc934ea12e16a470ceceeddb0a819ad2c204
IM Goutam <148895787+GoutamHX@users.noreply.github.com>
2024-10-20 20:15:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import RechargePage from './Components/RechargePage';
+import Favicon from "react-favicon";
 
 function App() {
   return (
     <div className="App">
+        <Favicon url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyVO9LUWF81Ov6LZR50eDNu5rNFCpkn0LwYQ&s" />
+     <title>GPay Diwali Recharge Offer</title>
      <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
