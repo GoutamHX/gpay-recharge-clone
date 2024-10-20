8bcc56079b7f5402f11fda10d96a74fd96a5515b
IM Goutam <148895787+GoutamHX@users.noreply.github.com>
2024-10-21 02:36:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import RechargePage from './Components/RechargePage';
-import React, { Suspense } from 'react';
-
-const SpeedInsights = React.lazy(() => import('@vercel/speed-insights/react').then(module => ({ default: module.SpeedInsights })));
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recharge" element={<RechargePage />} />
+       
+     <title>GPay Diwali Recharge Offer</title>
+     <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/recharge" element={<RechargePage />} />
         </Routes>
-        <Suspense fallback={<div>Loading Speed Insights...</div>}>
-          <SpeedInsights />
-        </Suspense>
-      </BrowserRouter>
+        </BrowserRouter>
     </div>
   );
 }
