4c52e06a99ea930a5c73962cd1994da49692604f
IM Goutam <148895787+GoutamHX@users.noreply.github.com>
2024-10-21 12:05:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import RechargePage from './Components/RechargePage';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
   return (
     <div className="App">     
-     <title>GPay Diwali Recharge Offer</title>
      <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/recharge" element={<RechargePage />} />
         </Routes>
-        <SpeedInsights />
         </BrowserRouter>
     </div>
   );
