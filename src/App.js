0250bf6662c3f1d4601ee0631077a90a6daf5a86
IM Goutam <148895787+GoutamHX@users.noreply.github.com>
2024-11-08 17:00:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import Home from './Components/Home';
 import RechargePage from './Components/RechargePage';
 function App() {
   return (
     <div className="App">     
+        <Analytics />
      <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
